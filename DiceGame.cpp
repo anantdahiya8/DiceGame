a39d2dbdc033a19b033a09524345426729f1b58d
anantdahiya8 <theanantdahiya@gmail.com>
2022-04-07 10:27:15 +0530

    Update DiceGame.cpp

--- DiceGame.cpp
+++ DiceGame.cpp
@@ -23,10 +23,21 @@ void displayLeaderBoard(){
     
 }
 
+void initializeTurn(vector<int> &turn){
+    int n = turn.size();
+    for(int i=0;i<n;i++){
+        turn[i] = i;
+    }
+    for(int i=0;i<n;i++){
+        int num = rand() % n;
+        swap(turn[i], turn[num]);
+    }
+}
+
 int main()
 {
     
-    int n , points;
+    int n, points, target;
     cout<<"Enter number of Players (Atleast 2 Players): ";
     cin>>n;
     if(n < 2){
@@ -35,6 +46,8 @@ int main()
     }
     cout<<"Enter number of points needed to win (Greater than 0): ";
     cin>>points;
+    cout<<"Enter target val for double points : ";
+    cin>>target;
     if(points <= 0){
         cout<<"!!Invalid Number!!";
         return 0;
@@ -45,25 +58,31 @@ int main()
     displayLeaderBoard();
 
     vector<int> turn(n, 0);
-    for(int i=0;i<n;i++){
-        turn[i] = i;
-    }
-    for(int i=0;i<n;i++){
-        int num = rand() % n;
-        swap(turn[i], turn[num]);
-    }
+    initializeTurn(turn);
 
-    vector<int> last(n, -1);
+    vector<int> lastDiceValue(n, -1);
     int current_turn = turn[0];
-    int counter = 0;
-    vector<bool> miss(n, false);
+    int turnCounter = 0;
+    vector<bool> check_turn(n, false);
+    vector<pair<int, int>> lastTwoDiceValue(n, {INT_MIN, INT_MIN});
     while(winner.size() < n){
-        if(counter == n){
-            counter = 0;
+        // for(int i=0;i<last.size();i++){
+        //     cout<<last[i]<<" ";
+        // }
+        // cout<<endl;
+        if(turnCounter == n){
+            turnCounter = 0;
         }
-        current_turn = turn[counter];
+        current_turn = turn[turnCounter];
         if(leaderBoard[current_turn] >= points){
-            counter += 1;
+            turnCounter += 1;
+            continue;
+        }
+
+        if(check_turn[current_turn]){
+            cout<<endl<<"!!! Skipping Player "<<current_turn + 1<<"  !!!"<<endl;
+            check_turn[current_turn] = false;
+            turnCounter += 1;
             continue;
         }
 
@@ -73,26 +92,36 @@ int main()
         if(ch != 'r'){
             return 0;
         }     
-        int dice = rand()%6 + 1;
+        //int dice = rand()%6 + 1;
+        int dice = 4;
         cout<<endl<<"Dice Value   :  "<<dice<<endl;
-        if(last[current_turn] == 1 && dice == 1){
+        int sumthree = dice + lastTwoDiceValue[current_turn].first + lastTwoDiceValue[current_turn].second;
+        swap(lastTwoDiceValue[current_turn].first, lastTwoDiceValue[current_turn].second);
+        lastTwoDiceValue[current_turn].first = dice; 
+
+
+        if(lastDiceValue[current_turn] == 1 && dice == 1){
             // Skip Turn
-            last[current_turn] == 1;
-            cout<<endl<<"!!! Skipping Player "<<current_turn + 1<<"  !!!";
+            lastDiceValue[current_turn] = -1;
+            check_turn[current_turn] = true;
+            leaderBoard[current_turn] += dice;  
         }else if(dice == 6){
-            last[current_turn] = 6;
+            lastDiceValue[current_turn] = 6;
             leaderBoard[current_turn] += dice;
-            counter -= 1;
+            turnCounter -= 1;
         }else{
-            last[current_turn] = dice;
+            lastDiceValue[current_turn] = dice;
             leaderBoard[current_turn] += dice;   
+        }
+        if(sumthree == target){
+            leaderBoard[current_turn] += dice;
         }
         if(leaderBoard[current_turn] >= points){
             winner.push_back(current_turn);
         }
 
         displayLeaderBoard();
-        counter += 1;
+        turnCounter += 1;
     }
 
 
